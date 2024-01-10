8086fe62a6590c21d1aa19b7c134227a19e4a6fe
akash <akash@DESKTOP-IEAL13F>
2024-01-10 15:43:04 +0530

    get new one

---
+++ src/test/java/function1/Child_1.java
@@ -0,0 +1,13 @@
+package function1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Child_1 {
+	@Test
+	public void test1()
+	{
+		Reporter.log("Executing test1....",true);
+	}
+
+}
---
+++ src/test/java/function1/Parent.java
@@ -0,0 +1,20 @@
+package function1;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Parent {
+	
+	@BeforeMethod
+	public void bm()
+	{
+		
+	}
+	
+	@AfterMethod
+	public void am()
+	{
+		
+	}
+
+}
